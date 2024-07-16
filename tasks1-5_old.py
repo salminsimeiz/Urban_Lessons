99700aa3364cdf63699b39b43c060131fe249e53
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-07-16 14:09:18 +0200

    Add files via upload

---
+++ tasks1-5_old.py
@@ -0,0 +1,30 @@
+# Длина слова
+a = len(input("word? "))
+print(f"Длина слова {a} знаков")
+
+# Суммы и разности
+first = int(input("Введите первое число "))
+second = int(input("Введите второе число "))
+summa = first + second
+diff = first - second
+print(f"Сумма равна {summa}, разность {diff}")
+
+# Среднее арифметическое
+first = float(input("Введите первое число "))
+second = float(input("Введите второе число "))
+third = float(input("Введите третье число "))
+mean = (first + second + third)/3
+print(f"Среднее арифметическое равно {mean}")
+
+# Простые строчки
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ", " + first_string)
+
+# Сложная формула
+a = float(input("Введите первое число "))
+b = float(input("Введите второе число "))
+c = float(input("Введите третье число "))
+f = (a*b) + (a*c)
+print(f**3/2)
+
