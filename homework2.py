f92bc8d4680745c8a687bed2d542116191694f80
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-07-17 16:54:40 +0200

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+num_of_tasks = 12
+num_of_hours_spent = 1.5
+name_of_course = "Python"
+time_for_task = num_of_hours_spent/num_of_tasks
+print(f'''Курс: {name_of_course}, всего задач: {num_of_tasks}, затрачено часов: {num_of_hours_spent},\
+среднее время выполнения: {time_for_task} часа.''')
