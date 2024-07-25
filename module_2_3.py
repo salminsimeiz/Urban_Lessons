79a08f47a154d0dbd9242f32f8b19ccad8d7492d
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-07-25 13:53:57 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -3,7 +3,12 @@ my_list = [int(number) for number in input("Введите числа через
 print(f"my list = {my_list}")
 i = 0
 while my_list[i] >= 0:
-    print(my_list[i])
-    i += 1
+
+    if my_list[i] == 0:
+        i +=1
+        continue
+    else:
+        print(my_list[i])
+        i += 1
     if i == len(my_list): break
 
