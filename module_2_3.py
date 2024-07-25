e2904bd4181362e3a3a6b4a4b0bcafef0da7c289
salminsimeiz <yarsalmin@gmail.com>
2024-07-25 12:14:50 +0200

    v1.0.0

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+# вводим исходный список
+my_list = [int(number) for number in input("Введите числа через пробел ").split()]
+print(f"my list = {my_list}")
+i = 0
+while my_list[i] >= 0:
+    print(my_list[i])
+    i += 1
+    if i == len(my_list): break
+
