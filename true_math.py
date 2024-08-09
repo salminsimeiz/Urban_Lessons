e33dc3bd52a0babecde4fe983bb80b7f8cf38ab2
salminsimeiz <yarsalmin@gmail.com>
2024-08-09 16:55:13 +0200

    v1.0.1

--- true_math.py
+++ true_math.py
@@ -1,11 +1,11 @@
 from math import inf
 
-def divide(first:int, second:int) -> float:
+
+def divide(first: int, second: int) -> float:
     if second == 0:
         return inf
     else:
         return first / second
-
 
 
 print(divide(15,0))
