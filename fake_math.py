2b72fc471bd5dcb30f29d0ed5a9764d40c0a87b8
salminsimeiz <yarsalmin@gmail.com>
2024-08-09 16:42:16 +0200

    v1.0.0

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first: int, second: int) -> float:
+    if second == 0:
+        return "Mistake"
+    else:
+        return first / second
+
+
+print(divide(3, 1))
--- module_3_3.py
+++ module_3_3.py
@@ -2,9 +2,6 @@ def print_params(a=1, b="hola", c=True):
     print(a, b, c)
 
 
-values_list = [56, "prego", ["s", "w", "o", "r", "d"]]
-values_dict = {"a": 1, "b": "stop", "c": False}
-values_list_2 = [(4.2,), (2, 3, 4)]
 
 print_params()
 print_params(b=25)
