d8dfd63493d4911ac1851b144af16877c44b7db7
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-08-16 17:54:43 +0300

    Add files via upload

---
+++ matches.py
@@ -0,0 +1,48 @@
+
+from math import acos
+from random import random
+import matplotlib.pyplot as plt
+
+
+def match_stat(m_length: float, gap: float, num: int) -> []:
+    """
+        Эта функция предназначена для решения одной задачи по статистике:
+    между двумя параллельными прямыми бросают спичку, длина которой больше расстояния между прямыми.
+    Нужно найти вероятность того,что спичка окажется между прямыми.
+        Очевидно,что эта вероятность пропорциональна расстоянию между прямыми и обратно пропорциональ-
+    на длине спички. Не решая задачу математическими методами, моделируем этот процесс и находим коэф-
+    фициент пропорциональности. Чем больше  проведенных экспериментов, тем точнее получается  значение
+    коэффициента ->1. Таким образом, измеряя геометрию  можно узнать вероятность, которая определяется
+    только соотношением gap/m_length.
+
+    :param m_length: длина спички
+    :param gap: расстояние между параллельными прямыми
+    :param num: число экспериментов (бросаний спички)
+    """
+    global x, y, simulated
+    while gap > 0:
+        n = 0
+        angle_min = acos(gap / m_length)
+        for i in range(num):
+            angle = acos(random())
+            if angle >= angle_min:
+                n += 1
+        x.append(gap / m_length)
+        y.append(n / num)
+        gap -= 0.2
+    simulated = x
+    return x, y, simulated
+
+
+x = []
+y = []
+simulated = []
+match_stat(8, 1.5, 1000)
+plt.plot(x, simulated, color="red")
+plt.scatter(x, y, color="green", marker="o")
+match_stat(8, 1.5, 50000)
+plt.scatter(x, y, marker="+")
+plt.xlabel("Относительная ширина щели")
+plt.ylabel("Вероятность")
+plt.title("Зависимость вероятности от геометрии")
+plt.show()
