f681d8e79037e2f216ec72582e968a47568cc806
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-10-13 19:41:59 +0300

    Add files via upload

--- module_10_5.py
+++ module_10_5.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import multiprocessing
+from threading import Thread
 
 
 def read_info(file_name):
@@ -11,29 +12,30 @@ def read_info(file_name):
                 all_data.append(line.rstrip())
                 continue
             else:
+                print(f"Завершена запись данных из файла {file_name}")
                 break
         return all_data
 
 
-"""filenames = [f'./file {number}.txt' for number in range(1, 5)]
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
 start = datetime.now()
 result = list(map(read_info, filenames))
 end = datetime.now()
 execution_time = end - start
-print(execution_time)"""
+print(execution_time)
 
-# execution_time = 0:00:10.639643
+# execution_time = 0:00:16.167920
 
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     filenames = [f'./file {number}.txt' for number in range(1, 5)]
     start = datetime.now()
     with multiprocessing.Pool(processes=4) as pool:
         result = list(pool.map(read_info, filenames))
     end = datetime.now()
     execution_time = end - start
-    print(execution_time)
+    print(execution_time)"""
 
-# execution_time = 0:00:17.003716
+# execution_time = 0:00:19.291658
 
 """filenames = [f'./file {number}.txt' for number in range(1, 5)]
 start = datetime.now()
@@ -42,4 +44,23 @@ end = datetime.now()
 execution_time = end - start
 print(execution_time)"""
 
-# execution_time = 0:00:10.534558
+# execution_time = 0:00:13.441628
+
+
+"""def main():
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    threads = [Thread(target=read_info, args=(filename,)) for filename in filenames]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    start = datetime.now()
+    main()
+    end = datetime.now()
+    execution_time = end - start
+    print(execution_time)"""
+
+# execution_time = 0:00:20.795973
