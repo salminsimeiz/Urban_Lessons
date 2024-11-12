fa556d7de34b02404a3833aa87b6f11a00056d2c
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2024-11-12 23:01:39 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+for i in range(1, 11):
+    age_ = i * 10
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", age_, 1000))
+
+cursor.execute("UPDATE Users SET balance = ? WHERE id % 2 != ?", ("500", 0))
+
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
+
+cursor.execute("SELECT username, email, age, balance  FROM Users WHERE age != ?", (60,))
+users = cursor.fetchall()
+for user in users:
+    print(f"Имя:{user[0]} | Почта:{user[1]} | Возраст:{user[2]} | Баланс:{user[3]}")
+
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+cursor.execute("SELECT COUNT(*) FROM Users")
+all_users = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+total_balances = cursor.fetchone()[0]
+print(total_balances / all_users)
+connection.commit()
+connection.close()
